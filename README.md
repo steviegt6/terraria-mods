b8c4736eb9f3a6799b13f45cf02e85f749f24dcb
Stephen T <xxlennygamerxx@gmail.com>
2025-02-03 22:58:39 -0600

    Set up basic project structure.

---
+++ README.md
@@ -0,0 +1,8 @@
+# terraria-mods
+
+Simple monorepository for my various Terraria mods.
+
+> [!INFO]
+> If you're looking for Nitrate, see [here](https://github.com/terraria-catalyst/nitrate-mod).
+
+More to come...
---
+++ src/SourceGenerator/GenerateInternalTypeInRootNamespaceGenerator.cs
@@ -0,0 +1,33 @@
+using System.Text;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+
+namespace Tomat.Terraria.ModLoader.SourceGenerator;
+
+[Generator]
+public sealed class GenerateInternalTypeInRootNamespaceGenerator : IIncrementalGenerator
+{
+    void IIncrementalGenerator.Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        context.RegisterSourceOutput(
+            context.CompilationProvider,
+            static (x, compilation) => x.AddSource(
+                "InternalTypeInRootNamespace.g.cs",
+                SourceText.From(MakeInternalType(compilation.AssemblyName ?? compilation.Assembly.Name), Encoding.UTF8)
+            )
+        );
+    }
+
+    private static string MakeInternalType(string rootNamespace)
+    {
+        var sb = new StringBuilder();
+        {
+            sb.AppendLine($"namespace {rootNamespace};");
+            sb.AppendLine($"");
+            sb.AppendLine($"[System.Runtime.CompilerServices.CompilerGenerated]");
+            sb.AppendLine($"internal static class Exists;");
+        }
+        return sb.ToString();
+    }
+}
