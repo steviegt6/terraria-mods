a10a45632f25d3c52530b4e62caa0da229644d9a
Stephen T <xxlennygamerxx@gmail.com>
2025-02-24 19:05:13 -0600

    Delete undeveloped LiquidLib project.

--- src/LiquidLib/Content/Liquid/Default/LavaLiquid.cs
+++
@@ -1,5 +0,0 @@
-using Tomat.Terraria.TML.LiquidLib.Common.Liquid;
-
-namespace Tomat.Terraria.TML.LiquidLib.Content.Liquid.Default;
-
-internal sealed class LavaLiquid : ModLiquid;
--- src/LiquidLib/LiquidMod.cs
+++
@@ -1,8 +0,0 @@
-using JetBrains.Annotations;
-
-using Terraria.ModLoader;
-
-namespace Tomat.Terraria.TML.LiquidLib;
-
-[UsedImplicitly(ImplicitUseKindFlags.InstantiatedWithFixedConstructorSignature)]
-public sealed class LiquidMod : Mod;
