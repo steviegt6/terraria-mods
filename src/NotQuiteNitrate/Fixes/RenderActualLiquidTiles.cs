39f4ecc10be53e0980bd3fc2e859c7a9118ea396
tomat <xxlennygamerxx@gmail.com>
2025-03-05 23:26:46 +0000

    feat(nqn): render liquid slopes on second liquid draw step as well (#1)
    * Experiment with fixing liquid slope rendering.
    
    * Discard work on the tile paint system extension which is frankly redundant,
    
    * Get rid of the now-irrelevant DrawNormalLiquids hook.
    
    * This isn't intentional but accidentalyl sort of fixes it?
    
    * Good enough?

--- src/NotQuiteNitrate/Fixes/RenderActualLiquidTiles.cs
+++ src/NotQuiteNitrate/Fixes/RenderActualLiquidTiles.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 using JetBrains.Annotations;
 
@@ -6,11 +8,14 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 using Terraria;
+using Terraria.GameContent;
 using Terraria.GameContent.Drawing;
 using Terraria.GameContent.Liquid;
 using Terraria.Graphics;
 using Terraria.ID;
 using Terraria.ModLoader;
+
+using Tomat.TML.Mod.NotQuiteNitrate.Utilities;
 
 namespace Tomat.TML.Mod.NotQuiteNitrate.Fixes;
 
@@ -21,87 +26,202 @@ namespace Tomat.TML.Mod.NotQuiteNitrate.Fixes;
 [UsedImplicitly(ImplicitUseKindFlags.InstantiatedWithFixedConstructorSignature)]
 internal sealed class RenderActualLiquidTiles : ModSystem
 {
+    private sealed class LiquidSlopeGenerator : ILoadable
+    {
+        // private readonly Dictionary<>
+
+        void ILoadable.Load(global::Terraria.ModLoader.Mod mod) { }
+
+        void ILoadable.Unload() { }
+    }
+
+    /*private readonly record struct LiquidSlopeKey(
+    )*/
+
     public override void Load()
     {
         base.Load();
 
-        // Discard drawing of partial triangles.
-        On_TileDrawing.DrawPartialLiquid += (
-            On_TileDrawing.orig_DrawPartialLiquid _,
-            TileDrawing                           _,
-            bool                                  _,
-            Tile                                  _,
-            ref Vector2                           _,
-            ref Rectangle                         _,
-            int                                   _,
-            ref VertexColors                      _
-        ) => { };
+        On_TileDrawing.DrawPartialLiquid += DrawPartialLiquid;
 
-        On_LiquidRenderer.DrawNormalLiquids += (
-            _,
-            self,
-            _,
-            drawOffset,
-            waterStyle,
-            globalAlpha,
-            isBackgroundDraw
-        ) =>
+        On_Main.DrawLiquid += DrawLiquid;
+    }
+
+    // Rewrite "partial liquid rendering" to use our generated liquid slope
+    // textures derived from the actual liquid textures instead of separate,
+    // ugly frames.
+    private static void DrawPartialLiquid(
+        On_TileDrawing.orig_DrawPartialLiquid orig,
+        TileDrawing                           self,
+        bool                                  behindBlocks,
+        Tile                                  tile,
+        ref Vector2                           position,
+        ref Rectangle                         liquidSize,
+        int                                   liquidType,
+        ref VertexColors                      colors
+    )
+    {
+        /*var renderer = LiquidRenderer.Instance;
+        var frameY   = renderer._animationFrame * 80;
+
+        var drawBehindBlock = behindBlocks && !TileID.Sets.BlocksWaterDrawingBehindSelf[tile.TileType];
+
+        var (x, y) = TilemapHelper.GetTilePosition(tile, Main.tile);
+        var tileAbove = Framing.GetTileSafely(x, y - 1);
+
+        var slope     = drawBehindBlock ? SlopeType.Solid : tile.Slope;
+        var smthAbove = tileAbove.HasTile || tileAbove.LiquidAmount > 0;
+        var slopeRect = slope switch
         {
-            Main.tileBatch.Begin();
+            SlopeType.Solid          => new Rectangle(16, smthAbove ? 48 : 0, 16, 16),
+            SlopeType.SlopeDownLeft  => new Rectangle(16, 48,                 16, 16),
+            SlopeType.SlopeDownRight => new Rectangle(16, 48,                 16, 16),
+            SlopeType.SlopeUpLeft    => new Rectangle(16, 48,                 16, 16),
+            SlopeType.SlopeUpRight   => new Rectangle(16, 48,                 16, 16),
+            _                        => throw new ArgumentOutOfRangeException(nameof(slope), slope, null),
+        };
+        {
+            slopeRect.Y += frameY;
+        }
+
+        var opacity = behindBlocks ? 1f : LiquidRenderer.DEFAULT_OPACITY[tile.LiquidType];
+        {
+            colors.BottomLeftColor  *= opacity;
+            colors.BottomRightColor *= opacity;
+            colors.TopLeftColor     *= opacity;
+            colors.TopRightColor    *= opacity;
+        }
+
+        Main.tileBatch.Draw(renderer._liquidTextures[liquidType].Value, position, slopeRect, colors, Vector2.Zero, 1f, SpriteEffects.None);
+
+        if (!behindBlocks)
+        {
+            Main.instance.TilesRenderer.DrawSingleTile(Main.instance.TilesRenderer._currentTileDrawInfo.Value, true, liquidType, Main.Camera.UnscaledPosition, Main.drawToScreen ? Vector2.Zero : new Vector2(Main.offScreenRange), x, y);
+        }*/
+
+        {
+            int  num  = tile.slope();
+            bool flag = !TileID.Sets.BlocksWaterDrawingBehindSelf[tile.type];
+            if (!behindBlocks)
             {
-                var drawArea = self._drawArea;
+                flag = false;
+            }
 
-                var idx = 0;
-                for (var x = drawArea.X; x < drawArea.X + drawArea.Width; x++)
-                for (var y = drawArea.Y; y < drawArea.Y + drawArea.Height; y++)
+            var opacity = flag ? 1f : LiquidRenderer.DEFAULT_OPACITY[tile.LiquidType];
+            {
+                colors.BottomLeftColor  *= opacity;
+                colors.BottomRightColor *= opacity;
+                colors.TopLeftColor     *= opacity;
+                colors.TopRightColor    *= opacity;
+            }
+
+            if (flag || num == 0)
+            {
+                Main.tileBatch.Draw(TextureAssets.Liquid[liquidType].Value, position, liquidSize, colors, default(Vector2), 1f, SpriteEffects.None);
+                goto DrawTile;
+            }
+            liquidSize.X += 18 * (num - 1);
+            switch (num)
+            {
+                case 1:
+                    Main.tileBatch.Draw(TextureAssets.LiquidSlope[liquidType].Value, position, liquidSize, colors, Vector2.Zero, 1f, SpriteEffects.None);
+                    break;
+
+                case 2:
+                    Main.tileBatch.Draw(TextureAssets.LiquidSlope[liquidType].Value, position, liquidSize, colors, Vector2.Zero, 1f, SpriteEffects.None);
+                    break;
+
+                case 3:
+                    Main.tileBatch.Draw(TextureAssets.LiquidSlope[liquidType].Value, position, liquidSize, colors, Vector2.Zero, 1f, SpriteEffects.None);
+                    break;
+
+                case 4:
+                    Main.tileBatch.Draw(TextureAssets.LiquidSlope[liquidType].Value, position, liquidSize, colors, Vector2.Zero, 1f, SpriteEffects.None);
+                    break;
+            }
+
+        DrawTile:
+            if (!behindBlocks)
+            {
+                var (x, y) = TilemapHelper.GetTilePosition(tile, Main.tile);
+                Main.instance.TilesRenderer.DrawSingleTile(Main.instance.TilesRenderer._currentTileDrawInfo.Value, true, liquidType, Main.Camera.UnscaledPosition, Main.drawToScreen ? Vector2.Zero : new Vector2(Main.offScreenRange), x, y);
+            }
+        }
+    }
+
+    // Modify liquid rendering to add an extra case for rendering liquids on top
+    // of tiles on the edges.
+    private static void DrawLiquid(
+        On_Main.orig_DrawLiquid orig,
+        Main                    self,
+        bool                    bg,
+        int                     waterStyle,
+        float                   alpha,
+        bool                    drawSinglePassLiquids
+    )
+    {
+        if (!Lighting.NotRetro)
+        {
+            self.oldDrawWater(bg, waterStyle, alpha);
+            return;
+        }
+
+        var stopwatch = new Stopwatch();
+        {
+            stopwatch.Start();
+        }
+
+        var drawOffset = (
+            Main.drawToScreen
+                ? Vector2.Zero
+                : new Vector2(Main.offScreenRange, Main.offScreenRange)
+        ) - Main.screenPosition;
+
+        if (bg)
+        {
+            self.TilesRenderer.DrawLiquidBehindTiles(waterStyle);
+        }
+        else
+        {
+            // Our extra rendering part, derived from DrawLiquidBehindTiles.
+            var pos = Main.Camera.UnscaledPosition;
+            var offScreen = Main.drawToScreen
+                ? Vector2.Zero
+                : new Vector2(Main.offScreenRange, Main.offScreenRange);
+
+            self.TilesRenderer.GetScreenDrawArea(
+                pos,
+                offScreen + (Main.Camera.UnscaledPosition - Main.Camera.ScaledPosition),
+                out var firstTileX,
+                out var lastTileX,
+                out var firstTileY,
+                out var lastTileY
+            );
+            {
+                for (var x = firstTileY; x < lastTileY + 4; x++)
+                for (var y = firstTileX - 2; y < lastTileX + 2; y++)
                 {
-                    var liquid = self._drawCache[idx++];
-
-                    if (!liquid.IsVisible)
+                    var tile = Main.tile[y, x];
                     {
-                        continue;
+                        // Specifically set solidLayer to true (only change).
+                        self.TilesRenderer.DrawTile_LiquidBehindTile(solidLayer: true, inFrontOfPlayers: false, waterStyle, pos, offScreen, y, x, tile);
                     }
-
-                    var sourceRectangle = liquid.SourceRectangle;
-                    if (liquid.IsSurfaceLiquid)
-                    {
-                        sourceRectangle.Y = 1280;
-                    }
-                    else
-                    {
-                        sourceRectangle.Y += self._animationFrame * 80;
-                    }
-
-                    var type    = (int)liquid.Type;
-                    var opacity = liquid.Opacity * (isBackgroundDraw ? 1f : LiquidRenderer.DEFAULT_OPACITY[type]);
-                    switch (type)
-                    {
-                        case LiquidID.Water:
-                            type = waterStyle;
-
-                            opacity *= globalAlpha;
-                            break;
-
-                        case LiquidID.Honey:
-                            type = 11;
-                            break;
-                    }
-
-                    opacity = Math.Min(1f, opacity);
-
-                    Lighting.GetCornerColors(x, y, out var vertices);
-                    {
-                        vertices.BottomLeftColor  *= opacity;
-                        vertices.BottomRightColor *= opacity;
-                        vertices.TopLeftColor     *= opacity;
-                        vertices.TopRightColor    *= opacity;
-                    }
-
-                    Main.DrawTileInWater(drawOffset, x, y);
-                    Main.tileBatch.Draw(self._liquidTextures[type].Value, new Vector2(x << 4, y << 4) + drawOffset + liquid.LiquidOffset, sourceRectangle, vertices, Vector2.Zero, 1f, SpriteEffects.None);
                 }
             }
-            Main.tileBatch.End();
-        };
+        }
+
+        LiquidRenderer.Instance.DrawNormalLiquids(Main.spriteBatch, drawOffset, waterStyle, alpha, bg);
+
+        if (drawSinglePassLiquids)
+        {
+            LiquidRenderer.Instance.DrawShimmer(Main.spriteBatch, drawOffset, bg);
+        }
+
+        if (!bg)
+        {
+            TimeLogger.DrawTime(4, stopwatch.Elapsed.TotalMilliseconds);
+        }
+
+        stopwatch.Stop();
     }
 }
