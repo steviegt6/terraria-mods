c0e6bc98ffe7c96e1cc3f2514b4b623ee8391c8e
Stephen T <xxlennygamerxx@gmail.com>
2025-03-22 01:37:05 -0500

    feat(ns): primitive asset replacement feature

---
+++ src/Nightshade/Common/Systems/AssetReplacement/AssetProvider.cs
@@ -0,0 +1,9 @@
+using ReLogic.Content;
+
+namespace Tomat.TML.Mod.Nightshade.Common.Systems.AssetReplacement;
+
+/// <summary>
+///     Provides a mutable reference to an asset.
+/// </summary>
+/// <typeparam name="T">The asset type.</typeparam>
+public delegate ref Asset<T> AssetProvider<T>() where T : class;
---
+++ src/Nightshade/Common/Systems/AssetReplacement/AssetReplacementHandle.cs
@@ -0,0 +1,48 @@
+using System;
+
+using ReLogic.Content;
+
+namespace Tomat.TML.Mod.Nightshade.Common.Systems.AssetReplacement;
+
+/// <summary>
+///     A handle to an asset replacement.  Upon creation, the original asset is
+///     replaced with the new asset.  Upon disposal, the original asset is
+///     restored.
+/// </summary>
+/// <typeparam name="T">The asset type.</typeparam>
+public sealed class AssetReplacementHandle<T> : IDisposable
+    where T : class
+{
+    /// <summary>
+    ///     The original asset to be replaced.
+    /// </summary>
+    public Asset<T> OriginalAsset { get; }
+
+    /// <summary>
+    ///     The new asset to replace the original asset.
+    /// </summary>
+    public Asset<T> NewAsset { get; }
+
+    private readonly AssetProvider<T> assetProvider;
+
+    public AssetReplacementHandle(
+        AssetProvider<T> assetProvider,
+        Asset<T>         newAsset
+    )
+    {
+        OriginalAsset      = assetProvider();
+        NewAsset           = newAsset;
+        this.assetProvider = assetProvider;
+
+        // This is the important part.  Set the value.
+        assetProvider() = newAsset;
+    }
+
+    /// <summary>
+    ///     Resets the replaced asset with the original asset once again.
+    /// </summary>
+    public void Dispose()
+    {
+        assetProvider() = OriginalAsset;
+    }
+}
