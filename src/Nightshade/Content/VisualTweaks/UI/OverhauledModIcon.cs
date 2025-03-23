147ce4cf4030c6617f6502bf544c61943aa9900a
Stephen T <xxlennygamerxx@gmail.com>
2025-03-23 01:51:28 -0500

    feat(ns): make uimoditem panels consistently black

---
+++ src/Nightshade/Content/VisualTweaks/UI/OverhauledModIcon.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Diagnostics;
+using System.Reflection;
+
+using JetBrains.Annotations;
+
+using Microsoft.Xna.Framework;
+
+using Terraria.ModLoader;
+using Terraria.ModLoader.UI;
+
+using Tomat.TML.Mod.Nightshade.Core.Attributes;
+
+namespace Tomat.TML.Mod.Nightshade.Content.VisualTweaks.UI;
+
+/// <summary>
+///     Reworks the rendering of the mod's <see cref="UIModItem"/> instance.
+/// </summary>
+[UsedImplicitly(ImplicitUseKindFlags.InstantiatedWithFixedConstructorSignature)]
+internal sealed class OverhauledModIcon : ILoadable
+{
+    [InitializedInLoad]
+    private static Mod? theMod;
+
+    void ILoadable.Load(global::Terraria.ModLoader.Mod mod)
+    {
+        if (mod is not Mod nsMod)
+        {
+            throw new InvalidOperationException("The mod instance is not of the correct type.");
+        }
+
+        theMod = nsMod;
+
+        MonoModHooks.Add(
+            GetMethod(nameof(UIModItem.OnInitialize)),
+            OnInitialize
+        );
+
+        MonoModHooks.Add(
+            GetMethod(nameof(UIModItem.SetHoverColors)),
+            SetHoverColors
+        );
+
+        return;
+
+        static MethodInfo GetMethod(string name)
+        {
+            return typeof(UIModItem).GetMethod(name, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance)!;
+        }
+    }
+
+    void ILoadable.Unload() { }
+
+    private static void OnInitialize(Action<UIModItem> orig, UIModItem self)
+    {
+        Debug.Assert(theMod is not null);
+
+        if (self._mod.Name != theMod.Name)
+        {
+            orig(self);
+            return;
+        }
+
+        self.BorderColor = Color.Black;
+
+        orig(self);
+    }
+
+    private static void SetHoverColors(
+        Action<UIModItem, bool> orig,
+        UIModItem               self,
+        bool                    hovered
+    )
+    {
+        Debug.Assert(theMod is not null);
+
+        if (self._mod.Name != theMod.Name)
+        {
+            orig(self, hovered);
+            return;
+        }
+
+        // Always set to black, we have our own effect for hovering.
+        self.BorderColor     = Color.Black;
+        self.BackgroundColor = new Color(20, 20, 20);
+
+        // TODO: Glow ring around the panel upon hovering?
+    }
+}
