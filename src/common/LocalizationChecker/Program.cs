0682da4d5bb197691226005bd50c39948b255fec
Stephen T <xxlennygamerxx@gmail.com>
2025-03-28 04:30:43 -0500

    feat(common): simple compile step to validate locale files

---
+++ src/common/LocalizationChecker/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+using Hjson;
+
+namespace LocalizationChecker;
+
+internal static class Program
+{
+    public static void Main(string[] args)
+    {
+        foreach (var file in args)
+        {
+            CheckFile(file);
+        }
+    }
+
+    private static void CheckFile(string file)
+    {
+        try
+        {
+            HjsonValue.Load(Path.GetFullPath(file));
+        }
+        catch (Exception e)
+        {
+            Console.Error.WriteLine("Failed to validate HJSON file with error: " + e);
+            Environment.ExitCode = 1;
+        }
+    }
+}
