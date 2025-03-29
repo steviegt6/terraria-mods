30391e360a45df2dd03dd999426c605d82a1154b
SpencerDawg <gayforunova@gmail.com>
2025-03-29 02:02:19 -0500

    quick and dirty way to force check for if file has changed since msbuild sux

--- src/common/ShaderCompiler/Program.cs
+++ src/common/ShaderCompiler/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 
 using Newtonsoft.Json;
@@ -27,6 +28,21 @@ internal static class Program
     }
 
     private static string baseDirectory = null!;
+
+private static byte[] GetFileHash(string fileName)
+{
+    HashAlgorithm sha1 = SHA1.Create();
+    if (sha1 is null)
+    {
+        throw new InvalidOperationException("SHA1 not supported");
+    }
+    if (!File.Exists(fileName))
+    {
+        throw new FileNotFoundException("File not found", fileName);
+    }
+    using(FileStream stream = new FileStream(fileName,FileMode.Open,FileAccess.Read))
+      return sha1.ComputeHash(stream);
+}
 
     public static void Main(string[] args)
     {
@@ -177,6 +193,41 @@ internal static class Program
 
         var hlslFile  = Path.Combine(basePath, realFileName + ".hlsl");
         var fxcOutput = Path.Combine(basePath, realFileName + ".fxc");
+        var hash = GetFileHash(hlslFile);
+        var hashString = BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+        
+        if (File.Exists(Directory.GetCurrentDirectory() + "/" + "changedFiles.txt"))
+        {
+            var changedFiles = File.ReadAllLines(Directory.GetCurrentDirectory() + "/" + "changedFiles.txt");
+            if (!changedFiles.Contains(hashString))
+            {
+                Console.WriteLine("File " + hlslFile + " has been changed, recompiling... " + $"({hashString})");
+                FileStream fs = new FileStream(Directory.GetCurrentDirectory() + "/" + "changedFiles.txt", FileMode.Append, FileAccess.Write);
+                using (StreamWriter sw = new StreamWriter(fs))
+                {
+                    sw.WriteLine(hashString);
+                }
+                fs.Close();
+            }
+            else
+            {
+                if (File.Exists(fxcOutput))
+                {
+                Console.WriteLine("File " + hlslFile + " has not been changed, skipping...");
+                return;
+                }
+            }
+        } else {
+            FileStream fs = new FileStream(Directory.GetCurrentDirectory() + "/" + "changedFiles.txt", FileMode.Create, FileAccess.Write);
+            using (StreamWriter sw = new StreamWriter(fs))
+            {
+
+                sw.WriteLine(hashString);
+            
+            }
+            fs.Close();
+        }
+
         var isLinux   = Environment.OSVersion.Platform == PlatformID.Unix;
         string fxcExePath = "";
         if (isLinux) {
