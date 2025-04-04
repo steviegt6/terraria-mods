92156c9ee0bebc30e7d9d1cb129a49f324f66cea
tomat <xxlennygamerxx@gmail.com>
2025-04-04 09:13:50 +0000

    feat(ns): dropped-in-world per-item (and per-npc) texture variants (#15)
    * feat(ns): add item variant sprites
    Currently not implemented in-game.
    
    * feat(ns): basic loading of item variant sprites
    
    * feat(ns): exclude file extension for support loading from assets
    
    * feat(ns): only load item variants on client
    
    * fix(ns): rename and add appropriate files for npc variants
    I think I got them all, but we may need to revisit this later or update our API to better support these declarations.
    
    * feat(ns): in-game items are assigned random ariants they are rendered with in the world
    
    * feat(ns): offset rendering for smaller-than-normal variant sprites
    
    * feat(ns): clear variant data on pickup

---
+++ src/nightshade/Nightshade/Common/Features/ItemVariants/ItemVariantRenderer.cs
@@ -0,0 +1,109 @@
+using JetBrains.Annotations;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameContent;
+using Terraria.ModLoader;
+
+namespace Nightshade.Common.Features.ItemVariants;
+
+/// <summary>
+///     Responsible for rendering and handling item variants in-game.
+/// </summary>
+[Autoload(Side = ModSide.Client)]
+[UsedImplicitly(ImplicitUseKindFlags.InstantiatedWithFixedConstructorSignature)]
+internal sealed class ItemVariantRenderer : GlobalItem
+{
+    public override bool InstancePerEntity => true;
+
+    private int npcId;
+    private int variantId;
+
+    public override void SetDefaults(Item entity)
+    {
+        base.SetDefaults(entity);
+
+        npcId     = 0;
+        variantId = 0;
+    }
+
+    public override bool PreDrawInWorld(Item item, SpriteBatch spriteBatch, Color lightColor, Color alphaColor, ref float rotation, ref float scale, int whoAmI)
+    {
+        if (npcId == 0 && variantId == 0)
+        {
+            return true;
+        }
+
+        if (!ItemVariantLoader.TryGetTextureForVariant(item.type, npcId, variantId, out var texturePath))
+        {
+            return true;
+        }
+
+        var texture = ModContent.Request<Texture2D>(texturePath).Value;
+
+        // Since our assets are purely visual, they are still restricted by the
+        // existing item hitbox parameters.  To fix this, we calculate the
+        // necessary offset and adjust rendering that way.  These textures don't
+        // exist on the server and we don't provide syncing guarantees, so we
+        // should not and will not modify the actual hitbox values.
+        // Origin is in the top left corner, so only handle cases where the
+        // sprite is shorter than vanilla's for now.
+        // TODO: Will we need to support cases where it's larger?
+        // TODO: Handle cases for framed animations when the time comes.
+
+        Main.instance.LoadItem(item.type);
+        var vanillaTexture = TextureAssets.Item[item.type];
+        var difference     = vanillaTexture.Height() - texture.Height;
+
+        var offset = new Vector2(0, difference > 0 ? difference : 0);
+        spriteBatch.Draw(texture, item.position - Main.screenPosition + offset, null, lightColor, rotation, Vector2.Zero, scale, SpriteEffects.None, 0f);
+
+        return false;
+    }
+
+    public override void OnSpawn(Item item, IEntitySource source)
+    {
+        base.OnSpawn(item, source);
+
+        // We can also just check for EntitySource_Parent if it's a concern.
+        if (source is not EntitySource_Loot lootSource)
+        {
+            return;
+        }
+
+        // TODO: If we want to support not always using the NPC variant, we need
+        //       to make GetVariant provide both of these values.
+        npcId     = lootSource.Entity is NPC npc ? npc.type : 0;
+        variantId = ItemVariantLoader.GetVariant(item.type, npcId);
+    }
+
+    public override bool CanStackInWorld(Item destination, Item source)
+    {
+        // If trying to merge with a dropped item of the same type, allow it to
+        // merge when the other item has no variant.
+        var otherRenderer = destination.GetGlobalItem<ItemVariantRenderer>();
+        if (otherRenderer.npcId == 0 && otherRenderer.variantId == 0)
+        {
+            npcId     = 0;
+            variantId = 0;
+        }
+
+        return base.CanStackInWorld(destination, source);
+    }
+
+    public override bool OnPickup(Item item, Player player)
+    {
+        // Clear the variant data on pickup since once in the inventory the data
+        // is no longer relevant.  It'd be cool to support preserving this data,
+        // but it has weird interactions when stacking such that it overrides
+        // incoming variant data and would look weird in-game (unexpected
+        // results), so let's not bother.
+        npcId     = 0;
+        variantId = 0;
+
+        return base.OnPickup(item, player);
+    }
+}
