5776c062cb79276afec215e5edf339a537300b68
Stephen T <xxlennygamerxx@gmail.com>
2025-04-29 00:44:06 -0500

    feat(ns): coconute chest (unfinished)

---
+++ src/nightshade/Nightshade/Content/Tiles/_Furniture/CoconutChestTile.cs
@@ -0,0 +1,8 @@
+namespace Nightshade.Content.Tiles;
+
+// TODO: Dust, name, other properties.
+
+internal sealed class CoconutChestTile : AbstractChest
+{
+    public override string Texture => Assets.Images.Tiles.Furniture.CoconutChestTile.KEY;
+}
