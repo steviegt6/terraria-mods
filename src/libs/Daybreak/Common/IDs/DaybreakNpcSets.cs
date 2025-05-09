ef33ba96b84a6b5cd34cc089ed76f015d284dd97
Stephen T <xxlennygamerxx@gmail.com>
2025-05-08 22:46:17 -0500

    feat(db): introduce various npc sets for tweaking (mostly town npc) behavior

---
+++ src/libs/Daybreak/Common/IDs/DaybreakNpcSets.cs
@@ -0,0 +1,97 @@
+using MonoMod.Cil;
+
+using Terraria;
+using Terraria.GameContent.Events;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Daybreak.Common.IDs;
+
+// TODO: NPCSmartInteractCandidateProvider.ProvideCandidate, CanChat
+// TODO: ShopHelper.GetNearbyResidentNPCs - what does it care about?
+// TODO: Support custom hook for EmoteBubble.CheckForNPCsToReactToEmoteBubble
+// TODO: Party of Doom Main.DoUpdateInWorld
+
+public sealed class DaybreakNpcSets : ModSystem
+{
+    public static bool?[] CanParty = [];
+
+    public static bool?[] ContributesToEocSpawn = [];
+
+    public static bool?[] ContributesToTravelingMerchantSpawn = [];
+
+    public static bool?[] TravelingMerchantCanSpawnNear = [];
+
+    public static bool?[] VulnerableToAfterPartyOfDoom = [];
+
+    public override void ResizeArrays()
+    {
+        base.ResizeArrays();
+        
+        CanParty = CreateSet<bool?>(nameof(CanParty), null);
+        ContributesToEocSpawn = CreateSet<bool?>(nameof(ContributesToEocSpawn), null);
+        ContributesToTravelingMerchantSpawn = CreateSet<bool?>(nameof(ContributesToTravelingMerchantSpawn), null);
+        TravelingMerchantCanSpawnNear = CreateSet<bool?>(nameof(TravelingMerchantCanSpawnNear), null);
+        VulnerableToAfterPartyOfDoom = CreateSet<bool?>(nameof(VulnerableToAfterPartyOfDoom), null);
+
+        return;
+        
+        T[] CreateSet<T>(string name, T defaultState)
+        {
+            return NPCID.Sets.Factory.CreateNamedSet(Mod, name)
+                        .RegisterCustomSet<T>(defaultState);
+        }
+    }
+
+    public override void Load()
+    {
+        base.Load();
+
+        On_BirthdayParty.CanNPCParty += ConsiderWhetherNpcCanParty;
+        IL_Main.UpdateTime_StartNight += ConsiderWhetherNpcLetsEocSpawn;
+        IL_Main.UpdateTime += ConsiderWhetherNpcLetsTravelingMerchantSpawn;
+        IL_WorldGen.SpawnTravelNPC += ConsiderWhetherTravelingMerchantCanSpawnNearNpc;
+        IL_Main.DoUpdateInWorld += ConsiderVulnerabilityToAfterPartyOfDoom;
+    }
+
+    private static bool ConsiderWhetherNpcCanParty(On_BirthdayParty.orig_CanNPCParty orig, NPC n)
+    {
+        return CanParty[n.type] ?? orig(n);
+    }
+
+    private static void ConsiderWhetherNpcLetsEocSpawn(ILContext il)
+    {
+        var c = new ILCursor(il);
+
+        c.GotoNext(MoveType.Before, x => x.MatchLdfld<NPC>(nameof(NPC.townNPC)));
+        c.Remove();
+        c.EmitDelegate(static (NPC npc) => ContributesToEocSpawn[npc.type] ?? npc.townNPC);
+    }
+    
+    private static void ConsiderWhetherNpcLetsTravelingMerchantSpawn(ILContext il)
+    {
+        var c = new ILCursor(il);
+        
+        c.GotoNext(MoveType.Before, x => x.MatchLdfld<NPC>(nameof(NPC.townNPC)));
+        c.Remove();
+        c.EmitDelegate(static (NPC npc) => ContributesToTravelingMerchantSpawn[npc.type] ?? npc.townNPC);
+    }
+    
+    private static void ConsiderWhetherTravelingMerchantCanSpawnNearNpc(ILContext il)
+    {
+        var c = new ILCursor(il);
+        
+        c.GotoNext(MoveType.Before, x => x.MatchLdfld<NPC>(nameof(NPC.townNPC)));
+        c.Remove();
+        c.EmitDelegate(static (NPC npc) => TravelingMerchantCanSpawnNear[npc.type] ?? npc.townNPC);
+    }
+    
+    private static void ConsiderVulnerabilityToAfterPartyOfDoom(ILContext il)
+    {
+        var c = new ILCursor(il);
+        
+        c.GotoNext(MoveType.Before, x => x.MatchLdfld<NPC>(nameof(NPC.townNPC)));
+        c.Remove();
+        c.EmitDelegate(static (NPC npc) => VulnerableToAfterPartyOfDoom[npc.type] ?? npc.townNPC);
+    }
+}
