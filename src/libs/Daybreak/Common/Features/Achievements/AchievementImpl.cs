67c1960eb355d0e3e2cf4647433bef3497d5c034
Stephen T <xxlennygamerxx@gmail.com>
2025-05-13 03:08:45 -0500

    feat(db): basic reimpl of achievement text snippet

--- src/libs/Daybreak/Common/Features/Achievements/AchievementImpl.cs
+++ src/libs/Daybreak/Common/Features/Achievements/AchievementImpl.cs
@@ -1,4 +1,11 @@
+using System.Diagnostics.CodeAnalysis;
+
+using Microsoft.Xna.Framework;
+
+using Terraria;
+using Terraria.GameContent.UI.Chat;
 using Terraria.ModLoader;
+using Terraria.UI.Chat;
 
 namespace Daybreak.Common.Features.Achievements;
 
@@ -7,13 +14,80 @@ namespace Daybreak.Common.Features.Achievements;
 /// </summary>
 internal sealed class AchievementImpl : ModSystem
 {
-    public static void Register(Achievement achievement)
+    private sealed class CompatibleAchievementSnippet : TextSnippet
     {
-        
+        private Achievement achievement;
+
+        public CompatibleAchievementSnippet(Achievement achievement) : base(achievement.DisplayName.Value, Color.LightBlue)
+        {
+            CheckForHover = true;
+            this.achievement = achievement;
+        }
+
+        public override void OnClick()
+        {
+            base.OnClick();
+
+            IngameOptions.Close();
+            // TODO: IngameFancyUI.OpenAchievementsAndGoto(achievement);
+        }
     }
 
-    public static void RegisterCategory(AchievementCategory category)
+    public override void Load()
     {
-        
+        base.Load();
+
+        On_AchievementTagHandler.Terraria_UI_Chat_ITagHandler_Parse += UseCompatibleTextSnippetForAchievementTag;
     }
+
+    private static TextSnippet UseCompatibleTextSnippetForAchievementTag(
+        On_AchievementTagHandler.orig_Terraria_UI_Chat_ITagHandler_Parse orig,
+        AchievementTagHandler self,
+        string text,
+        Color baseColor,
+        string options
+    )
+    {
+        if (!TrySplitName(text, out var modName, out var achName))
+        {
+            return VanillaAchievements.VANILLA_ACHIEVEMENTS_BY_NAME.TryGetValue(text, out var vanillaAch)
+                ? new CompatibleAchievementSnippet(vanillaAch)
+                : new TextSnippet(text);
+        }
+
+        if (!ModLoader.TryGetMod(modName, out var mod))
+        {
+            return new TextSnippet(text);
+        }
+
+        if (!mod.TryFind<Achievement>(achName, out var ach))
+        {
+            return new TextSnippet(text);
+        }
+
+        return new CompatibleAchievementSnippet(ach);
+
+        static bool TrySplitName(
+            string name,
+            [NotNullWhen(returnValue: true)] out string? domain,
+            [NotNullWhen(returnValue: true)] out string? subName
+        )
+        {
+            try
+            {
+                ModContent.SplitName(name, out domain, out subName);
+                return true;
+            }
+            catch
+            {
+                domain = null;
+                subName = null;
+                return false;
+            }
+        }
+    }
+
+    public static void Register(Achievement achievement) { }
+
+    public static void RegisterCategory(AchievementCategory category) { }
 }
