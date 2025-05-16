d581023491734297c0b4356d532003d5478df482
Stephen T <xxlennygamerxx@gmail.com>
2025-05-15 23:14:13 -0500

    feat(ns/boots): implement dummy temporal vestige

---
+++ src/nightshade/Nightshade/Content/Items/Accessories/_BootTree/TemporalVestige.cs
@@ -0,0 +1,36 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Nightshade.Content.Items.Accessories;
+
+internal sealed class TemporalVestige : ModItem
+{
+    public override string Texture => "ModLoader/UnloadedItem";
+
+    public override void SetDefaults()
+    {
+        base.SetDefaults();
+
+        Item.accessory = true;
+    }
+
+    public override void UpdateEquip(Player player)
+    {
+        base.UpdateEquip(player);
+        
+        // TODO: other effects
+        player.GetJumpState<SandstormInABottleJump>().Enable();
+    }
+
+    public override void AddRecipes()
+    {
+        base.AddRecipes();
+
+        // TODO: other items
+        CreateRecipe()
+           .AddIngredient(ItemID.SandstorminaBottle)
+           .AddTile(TileID.TinkerersWorkbench)
+           .Register();
+    }
+}
