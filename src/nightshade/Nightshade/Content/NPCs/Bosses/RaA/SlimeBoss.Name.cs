cc15b659c9021913173b754f0ad9ed9a145c5a86
Stephen T <xxlennygamerxx@gmail.com>
2025-05-22 19:39:28 -0500

    feat(ns/reductio): basic starspeak rendering

--- src/nightshade/Nightshade/Content/NPCs/Bosses/RaA/SlimeBoss.Name.cs
+++ src/nightshade/Nightshade/Content/NPCs/Bosses/RaA/SlimeBoss.Name.cs
@@ -1,101 +1,25 @@
-using Daybreak.Common.Features.Hooks;
-
-using Terraria;
-using Terraria.Localization;
-using Terraria.ModLoader;
-
 namespace Nightshade.Content.NPCs.Bosses.RaA;
 
 partial class SlimeBoss
 {
-    private readonly record struct NameProfile(int NameCount, int TitleCount, int SubtitleCount)
-    {
-        public int NameIndex => Main.rand.Next(NameCount);
-
-        public int TitleIndex => Main.rand.Next(TitleCount);
-
-        public int SubtitleIndex => Main.rand.Next(SubtitleCount);
-
-        public string GetFullBossName()
-        {
-            return Mods.Nightshade.NPCs.SlimeBoss.FullBossName.GetTextValue(
-                GetBossName(),
-                GetBossSubtitle()
-            );
-        }
-
-        public string GetBossName()
-        {
-            return Mods.Nightshade.NPCs.SlimeBoss.BossName.GetTextValue(
-                GetName(NameIndex)
-            );
-        }
-
-        public string GetBossSubtitle()
-        {
-            return Mods.Nightshade.NPCs.SlimeBoss.BossSubtitle.GetTextValue(
-                GetSubtitle(SubtitleIndex)
-            );
-        }
-
-        private static LocalizedText GetName(int i)
-        {
-            return Mods.Nightshade.NPCs.SlimeBoss.BossNames.GetChildText(i.ToString());
-        }
-
-        private static LocalizedText GetTitle(int i)
-        {
-            return Mods.Nightshade.NPCs.SlimeBoss.BossTitles.GetChildText(i.ToString());
-        }
-
-        private static LocalizedText GetSubtitle(int i)
-        {
-            return Mods.Nightshade.NPCs.SlimeBoss.BossSubtitles.GetChildText(i.ToString());
-        }
-    }
-
-    private static NameProfile nameProfile;
-
-    [OnLoad]
-    private static void InitializeNameInjections()
-    {
-        InitNameProfile();
-
-        LanguageManager.Instance.OnLanguageChanged += InitNameProfile_OnLanguageChanged;
-
-        On_Lang.GetNPCName += (orig, id) =>
-        {
-            if (id != ModContent.NPCType<SlimeBoss>())
-            {
-                return orig(id);
-            }
-
-            var text = Language.GetText("Mods.Nightshade.NPCs.SlimeBoss.DummyEntryForNpcName");
-            {
-                text.SetValue(nameProfile.GetFullBossName());
-            }
-            return text;
-        };
-    }
-
-    private static void InitNameProfile_OnLanguageChanged(LanguageManager languageManager)
-    {
-        InitNameProfile();
-    }
-
-    private static void InitNameProfile()
-    {
-        nameProfile = new NameProfile(
-            NameCount: GetCount(Mods.Nightshade.NPCs.SlimeBoss.BossNames.KEY + '.'),
-            TitleCount: GetCount(Mods.Nightshade.NPCs.SlimeBoss.BossTitles.KEY + '.'),
-            SubtitleCount: GetCount(Mods.Nightshade.NPCs.SlimeBoss.BossSubtitles.KEY + '.')
-        );
-
-        return;
-
-        static int GetCount(string filter)
-        {
-            return Language.FindAll(Lang.CreateDialogFilter(filter)).Length;
-        }
-    }
+    // Lang.GetNPCName
+    // Lang.GetNPCNameValue
+    // NPC.GetTypeNetName
+    // NPC.TypeName
+    // NPC.GetFullnameByID
+    // NPC.GetFullNetName
+    
+    // Condition.NpcIsPresent - x
+    // BestiaryEntry.Enemy
+    // BestiaryEntry.TownNPC - x
+    // BestiaryEntry.Critter - x
+    // Lang.GetInvasionWaveText
+    // NPCDefinitionElement.GetPassedOptionElements - x
+    // NPC.CountKillForBannersAndDropThem - x
+    // NPC.SpawnSkeletron - x
+    
+    // UnlockableNPCEntryIcon.GetHoverText
+    // Main.MouseText_DrawBuffTooltip
+    // Main.DrawInfoAccs
+    // NPCDefinition.DisplayName - x
 }
---
+++ src/nightshade/Nightshade/Content/NPCs/Bosses/RaA/Starspeak.cs
@@ -0,0 +1,235 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+using Nightshade.Common.Features;
+
+using ReLogic.Graphics;
+
+using Terraria;
+using Terraria.GameContent;
+using Terraria.UI.Chat;
+using Terraria.Utilities;
+
+namespace Nightshade.Content.NPCs.Bosses.RaA;
+
+/// <summary>
+///     'Starspeak' is the name given to the abstract constellation rendering
+///     obfuscating text.
+/// </summary>
+internal static class Starspeak
+{
+    private sealed class Tag : ILoadableTagHandler<Tag>
+    {
+        private sealed class Snippet(string text, Color color) : TextSnippet(text, color)
+        {
+            private const int horizontal_margin = 4;
+
+            private static DynamicSpriteFont Font => FontAssets.MouseText.Value;
+
+            public override bool UniqueDraw(
+                bool justCheckingString,
+                out Vector2 size,
+                SpriteBatch spriteBatch,
+                Vector2 position = new(),
+                Color color = new(),
+                float scale = 1f
+            )
+            {
+                size = GetSize(Text, scale, out var margin);
+                if (justCheckingString)
+                {
+                    return true;
+                }
+
+                DrawText(
+                    spriteBatch,
+                    Font,
+                    Text,
+                    position,
+                    color,
+                    scale,
+                    margin,
+                    Main.LocalPlayer
+                );
+                return true;
+            }
+
+            private static Vector2 GetSize(string text, float scale, out float margin)
+            {
+                // margin applied to left and right sides
+                margin = scale * horizontal_margin;
+
+                var size = Font.MeasureString(text) * scale;
+                {
+                    size.X += margin * 2f; // both sides
+                }
+
+                return size;
+            }
+        }
+
+        public const string NAME = "starspeak";
+
+        public string[] TagNames { get; } = [NAME];
+
+        public TextSnippet Parse(string text, Color baseColor, string options)
+        {
+            return new Snippet(text, baseColor);
+        }
+    }
+
+    /// <summary>
+    ///     A 'starspeak' sentence, knowing its size and the normalized points
+    ///     in which stars may be drawn.
+    /// </summary>
+    /// <param name="Size">The width and height of the sentence.</param>
+    /// <param name="NormalPoints">The normalized star points.</param>
+    private readonly record struct Sentence(Vector2 Size, Vector2[] NormalPoints);
+
+    // Maps fonts to a hashmap of sentences denoted by a string hash.
+    private static readonly ConditionalWeakTable<DynamicSpriteFont, Dictionary<string, Sentence>> font_sentence_cache = [];
+
+    public static string GetBossNameTag()
+    {
+        return $"[{Tag.NAME}/{Mods.Nightshade.NPCs.SlimeBoss.DisplayName.GetTextValue()}]";
+    }
+
+    public static void DrawText(
+        SpriteBatch sb,
+        DynamicSpriteFont font,
+        string text,
+        Vector2 position,
+        Color color,
+        float scale,
+        float margin,
+        Player player
+    )
+    {
+        var sentence = GetSentence(font, text);
+
+        var size = font.MeasureString(text) * scale;
+
+        var drawArea = new Rectangle(
+            (int)position.X,
+            (int)position.Y,
+            (int)(size.X + margin * 2f),
+            (int)size.Y
+        );
+
+        // Draw constellation.
+        {
+            var points = sentence.NormalPoints;
+
+            var starCount = points.Length;
+            for (var i = 0; i < starCount; i++)
+            {
+                var point = points[i];
+                var x = drawArea.X + (int)(point.X * drawArea.Width);
+                var y = drawArea.Y + (int)(point.Y * drawArea.Height);
+
+                var star = new Rectangle(x, y, (int)(4 * scale), (int)(4 * scale));
+                var color2 = Color.White;
+                if (i == 0)
+                {
+                    color2 = Color.Red;
+                }
+                else if (i == starCount - 1)
+                {
+                    color2 = Color.Blue;
+                }
+
+                // Draw star.
+                sb.Draw(
+                    TextureAssets.MagicPixel.Value,
+                    star,
+                    null,
+                    color2,
+                    0f,
+                    Vector2.Zero,
+                    SpriteEffects.None,
+                    0
+                );
+            }
+        }
+
+        // Draw text.
+        if (PlayerKnowsStarspeak(player)) { }
+    }
+
+    private static Sentence GetSentence(DynamicSpriteFont font, string text)
+    {
+        if (font_sentence_cache.TryGetValue(font, out var sentences))
+        {
+            if (sentences.TryGetValue(text, out var sentence))
+            {
+                return sentence;
+            }
+        }
+        else
+        {
+            sentences = new Dictionary<string, Sentence>();
+            font_sentence_cache.Add(font, sentences);
+        }
+
+        var size = font.MeasureString(text);
+        return sentences[text] = new Sentence(size, GetStarPoints(size.X, text.GetHashCode()));
+    }
+
+    private static Vector2[] GetStarPoints(float width, int seed)
+    {
+        // Guaranteed to produce at least 2 stars (clamped at 10 for now).  One
+        // star is added per 20 pixels of text.  Points are at first uniformly
+        // distributed (aside from the first and last points), before being
+        // offset randomly horizontally and vertically.
+
+        var rand = new FastRandom(seed);
+
+        var starCount = (int)(width / 20f);
+        starCount = Math.Clamp(starCount, 2, 10);
+
+        var points = new Vector2[starCount];
+        var step = width / (starCount - 1);
+        for (var i = 0; i < starCount; i++)
+        {
+            var x = step * i;
+            var y = rand.NextFloat();
+            points[i] = new Vector2(x, y);
+        }
+
+        // Clamp beginning and end stars so they reach the end of the sentence.
+        points[0].X = 0f;
+        points[^1].X = width;
+
+        // Distribute randomly horizontally.
+        if (starCount > 2)
+        {
+            for (var i = 1; i < starCount - 1; i++)
+            {
+                points[i].X += NextFloat(rand, -step, step) / 2f;
+            }
+        }
+
+        // Normalize horizontal values to [0, 1]
+        for (var i = 0; i < starCount; i++)
+        {
+            points[i].X /= width;
+        }
+
+        return points;
+
+        static float NextFloat(FastRandom rand, float minValue, float maxValue)
+        {
+            return (float)rand.NextDouble() * (maxValue - minValue) + minValue;
+        }
+    }
+
+    private static bool PlayerKnowsStarspeak(Player player)
+    {
+        // TODO
+        return true;
+    }
+}
