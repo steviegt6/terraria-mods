427578665f4c1bd88456c2449fe30186578c39b8
Dreitone <gayforunova@gmail.com>
2025-05-22 21:56:07 -0500

    consumer npc

--- src/nightshade/Nightshade/Content/NPCs/Bosses/TestBoss/TestBoss.cs
+++ src/nightshade/Nightshade/Content/NPCs/Bosses/TestBoss/TestBoss.cs
@@ -7,6 +7,7 @@ using System.Text.RegularExpressions;
 using Mono.Cecil.Cil;
 using System;
 using Terraria.WorldBuilding;
+using System.Collections.Generic;
 
 public class ConsumerOfSouls : ModNPC
 {
@@ -29,6 +30,89 @@ public class ConsumerOfSouls : ModNPC
         NPC.boss = true;
         NPC.noTileCollide = true;
         NPC.noGravity = true;
+    }
+}
+
+public class ConsumerOfSoulsNPC : ModNPC
+{
+    public override string Texture => Assets.Images.NPCs.ConsumerSheet.KEY;
+    public override string BossHeadTexture => Assets.Images.NPCs.ConsumerHead.KEY;
+
+    public override void SetStaticDefaults()
+    {
+        Main.npcFrameCount[NPC.type] = 24;
+
+        NPCID.Sets.ExtraFramesCount[NPC.type] = 5; // revisit later
+        NPCID.Sets.AttackFrameCount[NPC.type] = 4;
+        NPCID.Sets.DangerDetectRange[NPC.type] = 16 * 10; // length in tiles
+        NPCID.Sets.AttackType[NPC.type] = 0;
+        NPCID.Sets.AttackTime[NPC.type] = 100;
+        NPCID.Sets.AttackAverageChance[NPC.type] = 50;
+    }
+    
+    public override void SetDefaults()
+    {
+        base.SetDefaults();
+
+        NPC.townNPC = true;
+        NPC.friendly = true;
+        NPC.friendlyRegen = 10;
+
+        NPC.width = 30;
+        NPC.height = 44;
+
+        NPC.aiStyle = 7; // passive ai
+        AnimationType = NPCID.Dryad;
+
+        NPC.defense = 300;
+        NPC.damage = 300;
+
+        NPC.lifeMax = 300;
+        NPC.knockBackResist = 0.1f;
+    }
+    static List<string> chat = new List<string>
+    {
+        "My thrall was recently freed from his curse... are you the one who did it?",
+        "I can feel that one of my guardians has left this place. I'll have that old man's head.",
+        "Where did it go!? Where did it go... where did it go..."
+    };
+
+    static List<(Func<bool> predicate, string message)> SpecialInteractions = new()
+    {
+        new(() => NPC.AnyNPCs(NPCID.Clothier), "Ugh... I can still feel my connection to the host. Where is he?"),
+        new(() => NPC.AnyNPCs(NPCID.Mechanic), "Did you free our new convert? Suspicious..." ),
+        new(() => NPC.AnyNPCs(NPCID.Truffle), "The Dungeons could benefit from a faster way of cleaning a skeleton. Speaking of which..."),
+    };
+
+    public override string GetChat()
+    {
+        base.GetChat();
+
+        int randomNumber = Main.rand.Next();
+        var randomTuple = SpecialInteractions[randomNumber % SpecialInteractions.Count];
+        string chatText = randomTuple.predicate() ? randomTuple.message : chat[randomNumber % chat.Count];
+        return chatText;
+    }
+
+    public override void SetChatButtons(ref string button, ref string button2)
+    {
+        button = "Confess";
+        button2 = "Ask about the Dungeon";
+
+        base.SetChatButtons(ref button, ref button2);
+    }
+
+    public override void OnChatButtonClicked(bool firstButton, ref string shopName)
+    {
+        if (!firstButton)
+        {
+            Main.npcChatText = "Why would I tell you?";
+        }
+        else
+        {
+            NPC.NewNPC(NPC.GetSource_FromAI(), (int)NPC.position.X, (int)NPC.position.Y, ModContent.NPCType<ConsumerOfSouls>());
+        }
+        base.OnChatButtonClicked(firstButton, ref shopName);
     }
 }
 
