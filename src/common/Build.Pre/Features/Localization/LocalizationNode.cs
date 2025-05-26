3e484b6e44dc54f42c374ed4c60d1d4b0cdc60f1
Stephen T <xxlennygamerxx@gmail.com>
2025-05-26 04:43:12 -0500

    feat: localization generator

--- src/common/Build.Pre/Features/Localization/LocalizationNode.cs
+++ src/common/Build.Pre/Features/Localization/LocalizationNode.cs
@@ -1,5 +1,5 @@
 using System.Collections.Generic;
 
-namespace Tomat.Terraria.TML.SourceGenerator.Generators.DataDriven;
+namespace Build.Pre.Features.Localization;
 
 public sealed record LocalizationNode(string Name, Dictionary<string, LocalizationNode> Nodes, List<(string key, string value)> Keys);
