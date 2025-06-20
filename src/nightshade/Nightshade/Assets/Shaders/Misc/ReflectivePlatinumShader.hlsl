2b4e610e69daab3887cdefd8417d6de338e4aad5
Stephen T <xxlennygamerxx@gmail.com>
2025-06-20 04:17:26 -0500

    feat(ns/plat-critters): good-enough shader

--- src/nightshade/Nightshade/Assets/Shaders/Misc/ReflectivePlatinumShader.hlsl
+++ src/nightshade/Nightshade/Assets/Shaders/Misc/ReflectivePlatinumShader.hlsl
@@ -4,133 +4,15 @@ float2 uImageSize0;
 float3 uColor;
 float3 uLightSource;
 
-float4 uSourceRect;
-float uTime;
+float3 color1 = float3(101./255.,132./255.,174./255.) * 2.05;
+float3 color2 = float3(246./255.,216./255./1.5,235./255./1.5) * 2.5; // float3(0.75, 1., 0.75);
 
-float4 ArmorMirage(float4 v0 /* vertex color input */, float2 t0 /* texture coordinates (uv) */, sampler s0)
+float get_sat(float3 col)
 {
-    // preshader
-    float4 r0;
-    float4 c2;
-    float c0 = uTime;
-    float4 c1 = uSourceRect;
-    c2.xy = uImageSize0;
-
-    // rcp r0.x, c2.x
-    r0.x = 1.0f / c2.x;
-
-    // add c2.x, r0.x, r0.x
-    c2.x = r0.x + r0.x;
-
-    // rcp c0.x, c1.w
-    c0 = 1.0f / c1.w;
-
-    // mul c1.x, c0.x, (5)
-    c1.x = uTime * 5.0f;
-
-    r0 = float4(0.0f, 0.0f, 0.0f, 0.0f);
-
-    // ps_2_0
-    float4 c3 = uSourceRect;
-    float4 c4;
-    c4.xy = uImageSize0;
-
-    float4 r1;
-    float4 r2;
-
-    // def c5, -0.5, 30, 0.159154937, 0.5
-    float4 c5 = float4(-0.5f, 30.0f, 0.159154937f, 0.5f);
-
-    // def c6, 6.28318548, -3.14159274, 0, 1
-    float4 c6 = float4(6.28318548f, -3.14159274f, 0.0f, 1.0f);
-
-    // def c7, -1.55009923e-006, -2.17013894e-005, 0.00260416674, 0.00026041668
-    float4 c7 = float4(-1.55009923e-006f, -2.17013894e-005f, 0.00260416674f, 0.00026041668f);
-
-    // def c8, -0.020833334, -0.125, 1, 0.5
-    float4 c8 = float4(-0.020833334f, -0.125f, 1.0f, 0.5f);
-
-    // dcl v0
-    // #define v0 color
-
-    // dcl t0.xy
-    // #define t0 uv
-
-    // dcl_2d s0
-    // #define s0 uImage0
-
-    // mov r0.w, c4.y
-    r0.w = c4.y;
-
-    // mad r0.x, t0.y, r0.w, -c3.y
-    r0.x = t0.y * r0.w - c3.y;
-
-    // mov r1.xy, c5
-    r1.xy = c5.xy;
-
-    // mad r0.x, r0.x, c0.x, r1.x
-    r0.x = r0.x * c0.x + r1.x;
-
-    // mad r0.x, r0.x, r1.y, c1.x
-    r0.x = r0.x * r1.y + c1.x;
-
-    // mad r0.x, r0.x, c5.z, c5.w
-    r0.x = r0.x * c5.z + c5.w;
-
-#define frc(src) (src - floor(src))
-    // frc r0.x, r0.x
-    r0.x = frc(r0.x);
-
-    // mad r0.x, r0.x, c6.x, c6.y
-    r0.x = r0.x * c6.x + c6.y;
-
-    // sincos r1.y, r0.x, c7, c8
-    r1.y = sin(r0.x);
-
-    // mul r0.x, r1.y, c2.x
-    r0.x = r1.y * c2.x;
-
-    // mov r1.x, -r0.x
-    r1.x = -r0.x;
-
-    // mov r1.y, c6.z
-    r1.y = c6.z;
-
-    // add r1.xy, r1, t0
-    r1.xy = r1.xy + t0.xy;
-
-    // mov r0.y, c6.z
-    r0.y = c6.z;
-
-    // add r0.xy, r0, t0
-    r0.xy = r0.xy + t0.xy;
-
-    // texld r1, r1, s0
-    r1 = tex2D(s0, r1.xy);
-
-    // texld r0, r0, s0
-    r0 = tex2D(s0, r0.xy);
-
-    // texld r2, t0, s0
-    r2 = tex2D(s0, t0.xy);
-
-    // mov r0.x, r1.x
-    r0.x = r1.x;
-
-    // mov r0.w, c6.w
-    r0.w = c6.w;
-
-    // mov r0.y, r2.y
-    r0.y = r2.y;
-
-    // mul r0, r2.w, r0
-    r0 = r2.w * r0;
-
-    // mul r0, r0, v0
-    r0 = r0 * v0;
-
-    // mov oC0, r0
-    return r0;
+    float max_c = max(col.r, max(col.g, col.b));
+    float min_c = min(col.r, min(col.g, col.b));
+    float d = max_c - min_c;
+    return max_c > 0.0f ? (d / max_c) : 0.0f;
 }
 
 float4 ArmorReflectiveColor(float4 v0 /* vertex color input */, float2 t0 /* texture coordinates (uv) */, sampler s0)
@@ -168,10 +50,13 @@ float4 ArmorReflectiveColor(float4 v0 /* vertex color input */, float2 t0 /* tex
 
     // ps_2_0
 
+    float sat = get_sat(tex2D(s0, t0));
+    float3 theColor = sat > 0.125f ? color1 : color2;
+
     // uColor        c2  1
     // uLightSource  c3  1
     // uImage0       s0  1
-#define c2 uColor
+#define c2 theColor
 #define c3 uLightSource
     float4 r2;
     float4 r3;
@@ -323,7 +208,7 @@ float4 ArmorReflectiveColor(float4 v0 /* vertex color input */, float2 t0 /* tex
     r0.y = r0.y * c4.y;
 
     // mul r0.yzw, r0.y, c2.wzyx
-    r0.yzw = r0.y * float4(c2, 1.0f).wzyx;
+    r0.yzw = r0.y * c2.bgr;
 
     // mul r1.xyz, r0.wzyx, c6.x
     r1.xyz = r0.wzyx * c6.x;
