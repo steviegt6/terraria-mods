28a8dc7a2eddddd3d27c4bee9c7c9891a487f92b
Stephen T <xxlennygamerxx@gmail.com>
2025-07-10 04:16:59 -0500

    feat: remove RevisedConfiguration

--- src/libs/RevisedConfiguration/API/IO/ConfigFileManager.cs
+++
@@ -1,9 +0,0 @@
-namespace Tomat.TML.Lib.RevisedConfiguration.API.IO;
-
-/// <summary>
-///     Handles the management of config files.
-/// </summary>
-public sealed class ConfigFileManager
-{
-    
-}
