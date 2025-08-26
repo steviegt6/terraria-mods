fad81435666a739c8d58a05b2d6e3148cefb75ed
Stephen T <xxlennygamerxx@gmail.com>
2025-08-25 19:15:21 -0500

    feat: delete UnlockedZoom

--- src/mods/UnlockedZoom/Core/LocalizationReferences.cs
+++
@@ -1,9 +0,0 @@
-using Terraria.Localization;
-
-namespace Tomat.TML.Mod.UnlockedZoom.Core;
-
-// ReSharper disable MemberHidesStaticFromOuterClass
-internal static class LocalizationReferences
-{
-
-}
