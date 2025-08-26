f489603cd5b591a3c0859150b9451a996823663d
Stephen T <xxlennygamerxx@gmail.com>
2025-08-25 19:15:49 -0500

    feat: delete VanityTreeTest

--- src/mods/extra/VanityTreeTest/Core/AssetReferences.cs
+++
@@ -1,9 +0,0 @@
-#pragma warning disable CS8981
-
-namespace Tomat.TML.Mod.VanityTreeTest.Core;
-
-// ReSharper disable InconsistentNaming
-internal static class AssetReferences
-{
-
-}
--- src/mods/extra/VanityTreeTest/Core/IShaderParameters.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.Xna.Framework.Graphics;
-
-namespace Tomat.TML.Mod.VanityTreeTest.Core;
-
-internal interface IShaderParameters
-{
-    void Apply(EffectParameterCollection parameters);
-}
